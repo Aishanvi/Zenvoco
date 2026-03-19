b01968e5913dfa8ea105ff8ef425827faa507d73
Vegi Raju Anil Varma <24211a05le@bvrit.ac.in>
2026-03-19 17:54:25 +0530

    Updated code after deployment

--- zenvoco-backend/main.py
+++ zenvoco-backend/main.py
@@ -25,7 +25,12 @@ app = FastAPI(
 # Open Bridge for local UI development Cross-Origin boundaries
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173", 
+        "http://localhost:3000", 
+        "https://zenvoco.vercel.app"
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
